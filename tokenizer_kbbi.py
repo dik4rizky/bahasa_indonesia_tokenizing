2c0bd092ff653d98e8b933b913d90df976b88376
dik4rizky <160225103+dik4rizky@users.noreply.github.com>
2025-03-10 13:50:11 +0700

    first
    first

---
+++ tokenizer_kbbi.py
@@ -0,0 +1,26 @@
+import PyPDF2
+import re
+
+# Buka file PDF
+temp_term =[]
+term=[]
+halaman=0
+
+with open("Kamus-Besar-Bahasa-Indonesia.pdf", "rb") as file:
+    reader = PyPDF2.PdfReader(file)
+    for page in reader.pages:
+        teks=page.extract_text()  # Ekstrak teks dari setiap halaman
+        halaman+=1
+        print(halaman)
+        hasil = re.sub(r"[^a-zA-Z\s\-]", "", teks)
+        for kata in hasil.split():
+            temp_term.append(kata)
+            #print(kata)
+    
+term=sorted(list(set(temp_term)))    
+
+with open("output.txt", "w", encoding="utf-8", errors="ignore") as file:
+    for item in term:
+        file.write(item + "\n")
+
+        
